13402edad6729e4d91283a918361d758eb999a7f
Jammie <73665246+jammiehardy2@users.noreply.github.com>
2020-12-14 22:35:44 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,95 @@
-$(document).ready(function(){
-    function getdate(){
-      var today = moment().format("llll");
-     // var h = today.getHours();
-      var currentHour= moment().format("H");
-      var hoursArray = ["9 AM","10 AM","11 AM","12 PM"]
-    
-      for (var i =0; hoursArray.length; i++){
-        let tagTime=parsInt(hoursArray[i])+9;
-        if (tagTime>currentHour){
-          .past   
-        }
+var pastTime = document.getElementById("past");
+var currentTime = document.getElementById("present");
+var futureTime = document.getElementById("future");
+var saveText = document.getElementsByClassName("saveBtn");
+
+$(document).ready(function () {
+  function getdate() {
+    var today = moment().format("llll");
+    // var h = today.getHours();
+    var currentHour = moment().format("H");
+    var hoursArray = [
+      "9 AM",
+      "10 AM",
+      "11 AM",
+      "12 PM",
+      "1 PM",
+      "2 PM",
+      "3 PM",
+      "4 PM",
+      "5 PM",
+    ];
+
+    for (var i = 0; hoursArray.length; i++) {
+      let tagTime = parsInt(hoursArray[i]) + 9;
+      if (tagTime > currentHour) {
+        today = currentTime;
+      }
     }
-   
-  
-   $("button").click(getdate);
-  });
+
+    function getdate() {
+      var today = moment().format("llll");
+      // var h = today.getHours();
+      var pastTime = moment().format("H");
+      var hoursArray = [
+        "9 AM",
+        "10 AM",
+        "11 AM",
+        "12 PM",
+        "1 PM",
+        "2 PM",
+        "3 PM",
+        "4 PM",
+        "5 PM",
+      ];
+
+      for (var i = 0; hoursArray.length; i++) {
+        let tagTime = parsInt(hoursArray[i]) + 9;
+        if (tagTime > pastTime) {
+          today = pastTime;
+        }
+      }
+    }
+
+    function getdate() {
+      var today = moment().format("llll");
+      // var h = today.getHours();
+      var futureTime = moment().format("H");
+      var hoursArray = [
+        "9 AM",
+        "10 AM",
+        "11 AM",
+        "12 PM",
+        "1 PM",
+        "2 PM",
+        "3 PM",
+        "4 PM",
+        "5 PM",
+      ];
+
+      for (var i = 0; hoursArray.length; i++) {
+        let tagTime = parsInt(hoursArray[i]) + 9;
+        if (tagTime > futureTime) {
+          today = futureTime;
+        }
+      }
+    }
+  }
+
+    $(".saveBtn").click(function(event){
+      //event.stopPropagation()
+      
+      var usertext = $(this).siblings("textarea").val()
+      //console.log(usertext)
+      var key = $(this).attr("data-saving")
+      localStorage.setItem(key,usertext)
+    })
+
+    //saveText.addEventListener("click", function (event) {
+      //console.log("test");
+
+      //localStorage.setItem.on (click saveBtn);
+
+      //$("button").click(getdate);
+    //});
+});
